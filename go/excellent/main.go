1450df15f90d8ed10d2a4e5fa43a2a4cccc1a77e
sh0jitmy <shjtmy@gmail.com>
2025-09-28 17:09:14 +0900

    build error fix

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
+func main() {
+	number := 1
+	fmt.Println("%d even or Odd =%s",number,EvenOrOdd(number))
+}
+
 func EvenOrOdd(number int) string {
 	if number % 2 == 0 {
 		return "even"
